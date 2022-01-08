2cf2981279b180e8172ff97c5ae2bb3b52f0ca98
Heo Jaewon <yonseijaewon@gmail.com>
2022-01-08 23:21:05 +0900

    Add netlify settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+export default {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		}),
 		target: '#svelte'
 	}
 };
-
-export default config;
